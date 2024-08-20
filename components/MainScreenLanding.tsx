a4c24503bf1884b133109a85d684ebc6eb7613f1
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2024-08-20 22:24:07 +0530

    Update MainScreenLanding.tsx

--- components/MainScreenLanding.tsx
+++ components/MainScreenLanding.tsx
@@ -590,10 +590,9 @@ export function MainScreenLanding() {
   )
 }
 
-function CloudIcon(props: React.SVGProps<SVGSVGElement>) {
+function CloudIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -606,13 +605,13 @@ function CloudIcon(props: React.SVGProps<SVGSVGElement>) {
     >
       <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
     </svg>
-  );
+  )
 }
 
-function CodeIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function CodeIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -626,13 +625,13 @@ function CodeIcon(props: React.SVGProps<SVGSVGElement>) {
       <polyline points="16 18 22 12 16 6" />
       <polyline points="8 6 2 12 8 18" />
     </svg>
-  );
+  )
 }
 
-function DatabaseIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function DatabaseIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -647,13 +646,13 @@ function DatabaseIcon(props: React.SVGProps<SVGSVGElement>) {
       <path d="M3 5V19A9 3 0 0 0 21 19V5" />
       <path d="M3 12A9 3 0 0 0 21 12" />
     </svg>
-  );
+  )
 }
 
-function MailIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function MailIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -667,13 +666,13 @@ function MailIcon(props: React.SVGProps<SVGSVGElement>) {
       <rect width="20" height="16" x="2" y="4" rx="2" />
       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
     </svg>
-  );
+  )
 }
 
-function MapPinIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function MapPinIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -687,13 +686,13 @@ function MapPinIcon(props: React.SVGProps<SVGSVGElement>) {
       <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
       <circle cx="12" cy="10" r="3" />
     </svg>
-  );
+  )
 }
 
-function MountainIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function MountainIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -706,13 +705,14 @@ function MountainIcon(props: React.SVGProps<SVGSVGElement>) {
     >
       <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
     </svg>
-  );
+  )
 }
 
-function PhoneIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function PhoneIcon(any: props) {
   return (
     <svg
-      {...props}
+
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -725,13 +725,13 @@ function PhoneIcon(props: React.SVGProps<SVGSVGElement>) {
     >
       <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
     </svg>
-  );
+  )
 }
 
-function PowerIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function PowerIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -745,13 +745,13 @@ function PowerIcon(props: React.SVGProps<SVGSVGElement>) {
       <path d="M12 2v10" />
       <path d="M18.4 6.6a9 9 0 1 1-12.77.04" />
     </svg>
-  );
+  )
 }
 
-function RocketIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function RocketIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -767,13 +767,13 @@ function RocketIcon(props: React.SVGProps<SVGSVGElement>) {
       <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
       <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
     </svg>
-  );
+  )
 }
 
-function SmartphoneIcon(props: React.SVGProps<SVGSVGElement>) {
+
+function SmartphoneIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -787,13 +787,12 @@ function SmartphoneIcon(props: React.SVGProps<SVGSVGElement>) {
       <rect width="14" height="20" x="5" y="2" rx="2" ry="2" />
       <path d="M12 18h.01" />
     </svg>
-  );
+  )
 }
 
-function ComputerIcon(props: React.SVGProps<SVGSVGElement>) {
+function ComputerIcon(any: props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -804,23 +803,69 @@ function ComputerIcon(props: React.SVGProps<SVGSVGElement>) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <rect width="20" height="14" x="2" y="3" rx="2" ry="2" />
-      <path d="M8 21h8" />
-      <path d="M12 17v4" />
+      <rect width="14" height="8" x="5" y="2" rx="2" />
+      <rect width="20" height="8" x="2" y="14" rx="2" />
+      <path d="M6 18h2" />
+      <path d="M12 18h6" />
     </svg>
-  );
+  )
 }
 
-export {
-  CloudIcon,
-  CodeIcon,
-  DatabaseIcon,
-  MailIcon,
-  MapPinIcon,
-  MountainIcon,
-  PhoneIcon,
-  PowerIcon,
-  RocketIcon,
-  SmartphoneIcon,
-  ComputerIcon,
-};
+
+function InfoIcon(any: props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M12 16v-4" />
+      <path d="M12 8h.01" />
+    </svg>
+  )
+}
+
+function ArrowUpIcon(any: props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m5 12 7-7 7 7" />
+      <path d="M12 19V5" />
+    </svg>
+  )
+}
+
+function ArrowRightIcon(any: props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 12h14" />
+      <path d="M12 5l7 7-7 7" />
+    </svg>
+  )
+}
