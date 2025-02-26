8f6ad6497f29e15eba074bd1ba9ecc87d913d6dd
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2025-02-26 14:51:34 +0530

    Update layout.tsx

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -53,12 +53,10 @@ export default function RootLayout({
           name="msapplication-config"
           content="/favicon/browserconfig.xml"
         />
-        <meta name="theme-color" content="#000" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
-      <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-      >
+      <body className={cn(inter.className, "bg-white text-black")}>
         {/* <ThemeSwitcher /> */}
         <div className="min-h-screen">{children}</div>
         <Footer />
